541e59d3e0f182fa4cf3808b6c0867565f950d11
mbrons07 <mick.brons@outlook.com>
2026-09-21 13:02:32 +0200

    door slide werkt

--- Assets/Scripts/Interactables/Key.cs
+++ Assets/Scripts/Interactables/Key.cs
@@ -2,18 +2,34 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    private bool openDoor = false;
+    private float openTimer = 0f;
+    public float openDuration = 1f;
+    public float speed = 1f;
+
+    private void Update()
+    {
+        if (!openDoor)
+            return;
+
+        openTimer += Time.deltaTime;
+
+        if (openTimer >= openDuration)
+        {
+            openDoor = false;
+            return;
+        }
+
+        transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Key"))
+        if (collision.gameObject.CompareTag("Key") && !openDoor)
         {
-            Debug.Log("yo het fucking ding werkt");
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Door"))
-        {
-            Debug.Log("deur opened");
+            openDoor = true;
+            openTimer = 0f;
+            Debug.Log("Door opened");
         }
     }
 }
