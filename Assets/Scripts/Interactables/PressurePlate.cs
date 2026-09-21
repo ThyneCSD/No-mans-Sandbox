6c9c9ab390329922dd07aa3959a6c42fc9b87848
Thyne <Thyneschool@gmail.com>
2026-09-21 11:11:22 +0200

    Pressure plate toggle added
    Subaru is tuff

--- Assets/Scripts/Interactables/PressurePlate.cs
+++ Assets/Scripts/Interactables/PressurePlate.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 public class pressurePlate : MonoBehaviour
 {
     private bool isPressed = false;
-
-    private void Update()
-    {
-
-    }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Object"))
+        if (collision.gameObject.CompareTag("Object") && !isPressed)
         {
             Debug.Log("the thick thighs are on the pressure plate");
             isPressed = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Object") && isPressed)
+        {
+            Debug.Log("the thick thighs are NOT on the pressure plate");
+            isPressed = false;
         }
     }
 }
